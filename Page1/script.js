1bcfbbc06f3e73e5917e6d3b72531606f0fcb1e0
VenomRock3 <165355050+venomrock3-gamer@users.noreply.github.com>
2025-01-20 13:50:09 -0500

    Delete Page1 directory

--- Page1/script.js
+++
@@ -1,5 +0,0 @@
-alert('Hello! This website was created by Riaan Gandhi!');
-console.log('Alert is working!');
-var name;
-name = prompt('What is your name? ');
-console.log('The user\'s name is', name)

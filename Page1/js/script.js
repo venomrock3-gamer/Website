8201ab208b515e479968684b1d9fb89932f21ade
VenomRock3 <165355050+venomrock3-gamer@users.noreply.github.com>
2025-02-01 12:55:28 -0500

    Add files via upload

---
+++ Page1/js/script.js
@@ -0,0 +1,5 @@
+alert('Hello! This website was created by Riaan Gandhi!');
+console.log('Alert is working!');
+var name;
+name = prompt('What is your name? ');
+console.log('The user\'s name is', name)
